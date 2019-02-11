f3866c7df3dae1bd19cf81a7634386a3cc238ee1
androidZ200 <androidZ200daniilsamara@mail.ru>
2019-02-11 16:16:14 +0400

    Labyrinth class
    Added a new labyrinth class

--- cube maze/MainForm.cs
+++ cube maze/MainForm.cs
@@ -10,106 +10,20 @@ using System.Windows.Forms;
 
 namespace cube_maze
 {
-    public partial class Main : Form
+    public partial class MainForm : Form
     {
-        byte[,] currentMaze;
+        Labyrinth currentMaze;
         bool isPlaying = false;
         Point position = new Point();
-        Point finish = new Point();
-        Point start = new Point();
         Point imageSize = new Point();
         List<Point> neighbors = new List<Point>();
 
-        public Main()
+        public MainForm()
         {
             InitializeComponent();
-            maze2D();
+            currentMaze = new Labyrinth();
             imageSize = sizeImage();
             Draw();
-        }
-        void maze2D()
-        {
-            isPlaying = false;
-            Random rnd = new Random();
-            int x = rnd.Next(9, 13);
-            if (x % 2 == 0) x++;
-            int y = rnd.Next(15, 19);
-            byte[,] field = new byte[x, y];
-            int[,] group = new int[x, y];
-            int counter = 0;
-            for (int i = 0; i < x; i++)
-                for (int j = 0; j < y; j++)
-                {
-                    field[i, j] = 0;
-                    group[i, j] = counter;
-                    counter++;
-                }
-            while (true)
-            {
-                int X = rnd.Next(x);
-                int Y = rnd.Next(y);
-                int[] neighbors = new int[4];
-                for (int i = 0; i < 4; i++)
-                    if (i == 0)
-                        try { neighbors[i] = group[X, Y - 1]; }
-                        catch { neighbors[i] = -1; }
-                    else if (i == 1)
-                        try { neighbors[i] = group[X + 1, Y]; }
-                        catch { neighbors[i] = -1; }
-                    else if (i == 2)
-                        try { neighbors[i] = group[X, Y + 1]; }
-                        catch { neighbors[i] = -1; }
-                    else if (i == 3)
-                        try { neighbors[i] = group[X - 1, Y]; }
-                        catch { neighbors[i] = -1; }
-                bool link = false;
-                for (int i = 0; i < 4; i++)
-                    if (neighbors[i] != group[X, Y] && neighbors[i] != -1)
-                        link = true;
-                while (link)
-                {
-                    int rotate = rnd.Next(4);
-                    if (neighbors[rotate] != group[X, Y] && neighbors[rotate] != -1)
-                    {
-                        int ReplacingAGroup = neighbors[rotate];
-                        if (rotate == 0)
-                        {
-                            field[X, Y] += 1;
-                            field[X, Y - 1] += 4;
-                        }
-                        else if (rotate == 1)
-                        {
-                            field[X, Y] += 2;
-                            field[X + 1, Y] += 8;
-                        }
-                        else if (rotate == 2)
-                        {
-                            field[X, Y] += 4;
-                            field[X, Y + 1] += 1;
-                        }
-                        else if (rotate == 3)
-                        {
-                            field[X, Y] += 8;
-                            field[X - 1, Y] += 2;
-                        }
-                        for (int i = 0; i < x; i++)
-                            for (int j = 0; j < y; j++)
-                                if (group[i, j] == ReplacingAGroup)
-                                    group[i, j] = group[X, Y];
-                        link = false;
-                    }
-                }
-                link = true;
-                for (int i = 0; i < x; i++)
-                    for (int j = 0; j < y; j++)
-                        if (group[i, j] != group[X, Y])
-                            link = false;
-                if (link)
-                    break;
-            }
-            finish = new Point((x - 1) / 2, 0);
-            start = new Point((x - 1) / 2, y - 1);
-            currentMaze = field;
         }
         void Draw()
         {
@@ -117,18 +31,18 @@ namespace cube_maze
             {
                 Bitmap bmp = new Bitmap(imageSize.X, imageSize.Y);
                 Graphics g = Graphics.FromImage(bmp);
-                double Step = imageSize.X * 1.0 / currentMaze.GetLength(0);
+                double Step = imageSize.X * 1.0 / currentMaze.Width;
                 if (!isPlaying)
-                    g.FillRectangle(new SolidBrush(Color.Green), (float)(start.X * Step), (float)(start.Y * Step), (float)Step - 1, (float)Step - 1);
+                    g.FillRectangle(new SolidBrush(Color.Green), (float)(currentMaze.Start.X * Step), (float)(currentMaze.Start.Y * Step), (float)Step - 1, (float)Step - 1);
                 else
                 {
                     Brush b = new SolidBrush(Color.Gray);
-                    if (position == finish)
-                        g.FillRectangle(new SolidBrush(Color.Green), (float)(finish.X * Step), (float)(finish.Y * Step), (float)Step - 1, (float)Step - 1);
+                    if (position == currentMaze.Finish)
+                        g.FillRectangle(new SolidBrush(Color.Green), (float)(currentMaze.Finish.X * Step), (float)(currentMaze.Finish.Y * Step), (float)Step - 1, (float)Step - 1);
                     else g.FillRectangle(b, (float)(position.X * Step), (float)(position.Y * Step), (float)Step - 1, (float)Step - 1);
                     for (int i = 0; i < neighbors.Count; i++)
-                        if (neighbors[i] == finish)
-                            g.FillRectangle(new SolidBrush(Color.Green), (float)(finish.X * Step), (float)(finish.Y * Step), (float)Step - 1, (float)Step - 1);
+                        if (neighbors[i] == currentMaze.Finish)
+                            g.FillRectangle(new SolidBrush(Color.Green), (float)(currentMaze.Finish.X * Step), (float)(currentMaze.Finish.Y * Step), (float)Step - 1, (float)Step - 1);
                         else g.FillRectangle(b, (float)(neighbors[i].X * Step), (float)(neighbors[i].Y * Step), (float)Step - 1, (float)Step - 1);
                 }
                 pictureBox1.Image = bmp;
@@ -137,15 +51,15 @@ namespace cube_maze
         Point isPosition(Point Mouse)
         {
             Point padding = new Point((pictureBox1.Width - imageSize.X) / 2, (pictureBox1.Height - imageSize.Y) / 2);
-            double Step = imageSize.Y * 1.0 / currentMaze.GetLength(1);
+            double Step = imageSize.Y * 1.0 / currentMaze.Height;
             Point position = new Point(-1, -1);
-            for(int i = 1; i <= currentMaze.GetLength(0); i++)
+            for(int i = 1; i <= currentMaze.Width; i++)
                 if((i-1) * Step + padding.X < Mouse.X && Mouse.X <= i * Step + padding.X)
                 {
                     position.X = i - 1;
                     break;
                 }
-            for (int i = 1; i <= currentMaze.GetLength(1); i++)
+            for (int i = 1; i <= currentMaze.Height; i++)
                 if ((i - 1) * Step + padding.Y < Mouse.Y && Mouse.Y <= i * Step + padding.Y)
                 {
                     position.Y = i - 1;
@@ -155,20 +69,11 @@ namespace cube_maze
         }
         Point sizeImage()
         {
-            double XYmaze = currentMaze.GetLength(0) * 1.0 / currentMaze.GetLength(1);
+            double XYmaze = currentMaze.Width * 1.0 / currentMaze.Height;
             double XYpicture = pictureBox1.Width * 1.0 / pictureBox1.Height;
             if(XYpicture > XYmaze)
                 return new Point((int)(pictureBox1.Height * XYmaze), pictureBox1.Height);
             return new Point(pictureBox1.Width, (int)(pictureBox1.Width / XYmaze));
-        }
-        void UpdateNeighbors(Point position)
-        {
-            neighbors.Clear();
-            int t = currentMaze[position.X, position.Y];
-            if ((t & 8) != 0) neighbors.Add(new Point(position.X - 1, position.Y));
-            if ((t & 4) != 0) neighbors.Add(new Point(position.X, position.Y + 1));
-            if ((t & 2) != 0) neighbors.Add(new Point(position.X + 1, position.Y));
-            if ((t & 1) != 0) neighbors.Add(new Point(position.X, position.Y - 1));
         }
 
         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
@@ -181,7 +86,7 @@ namespace cube_maze
                     for (int i = 0; i < neighbors.Count; i++)
                         if (neighbors[i] == currentPosition)
                         {
-                            UpdateNeighbors(neighbors[i]);
+                            neighbors = currentMaze.GetNeighbors(neighbors[i]);
                             position = currentPosition;
                             Draw();
                             return;
@@ -194,17 +99,17 @@ namespace cube_maze
         private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
         {
             Point currentPosition = isPosition(e.Location);
-            if(!isPlaying && currentPosition == start)
+            if(!isPlaying && currentPosition == currentMaze.Start)
             {
                 isPlaying = true;
-                UpdateNeighbors(start);
-                position = start;
+                neighbors = currentMaze.GetNeighbors(currentMaze.Start);
+                position = currentMaze.Start;
                 Draw();
             }
-            else if(isPlaying && currentPosition == finish)
+            else if(isPlaying && currentPosition == currentMaze.Finish)
             {
                 MessageBox.Show("Победа!");
-                maze2D();
+                currentMaze = new Labyrinth();
                 imageSize = sizeImage();
                 Draw();
             }
---
+++ cube maze/Maze.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace cube_maze
+{
+    class Labyrinth
+    {
+        public byte[,] Maze { get; private set; }
+        public Point Finish { get; private set; }
+        public Point Start { get; private set; }
+        public int Height { get; private set; }
+        public int Width { get; private set; }
+
+        public Labyrinth()
+        {
+            GenerateMaze();
+        }
+        public List<Point> GetNeighbors(Point Position)
+        {
+            List<Point> neighbors = new List<Point>();
+            int t = Maze[Position.X, Position.Y];
+            if ((t & 8) != 0) neighbors.Add(new Point(Position.X - 1, Position.Y));
+            if ((t & 4) != 0) neighbors.Add(new Point(Position.X, Position.Y + 1));
+            if ((t & 2) != 0) neighbors.Add(new Point(Position.X + 1, Position.Y));
+            if ((t & 1) != 0) neighbors.Add(new Point(Position.X, Position.Y - 1));
+            return neighbors;
+        }
+
+        private void GenerateMaze()
+        {
+            Random rnd = new Random();
+            Width = rnd.Next(9, 13);
+            if (Width % 2 == 0) Width++;
+            Height = rnd.Next(15, 19);
+            byte[,] field = new byte[Width, Height];
+            int[,] group = new int[Width, Height];
+            for (int i = 0, counter = 0; i < Width; i++)
+                for (int j = 0; j < Height; j++)
+                {
+                    field[i, j] = 0;
+                    group[i, j] = counter;
+                    counter++;
+                }
+            while (true)
+            {
+                int X = rnd.Next(Width);
+                int Y = rnd.Next(Height);
+                int[] neighbors = new int[4];
+                for (int i = 0; i < 4; i++)
+                    if (i == 0)
+                        try { neighbors[i] = group[X, Y - 1]; }
+                        catch { neighbors[i] = -1; }
+                    else if (i == 1)
+                        try { neighbors[i] = group[X + 1, Y]; }
+                        catch { neighbors[i] = -1; }
+                    else if (i == 2)
+                        try { neighbors[i] = group[X, Y + 1]; }
+                        catch { neighbors[i] = -1; }
+                    else if (i == 3)
+                        try { neighbors[i] = group[X - 1, Y]; }
+                        catch { neighbors[i] = -1; }
+                bool link = false;
+                for (int i = 0; i < 4; i++)
+                    if (neighbors[i] != group[X, Y] && neighbors[i] != -1)
+                        link = true;
+                while (link)
+                {
+                    int rotate = rnd.Next(4);
+                    if (neighbors[rotate] != group[X, Y] && neighbors[rotate] != -1)
+                    {
+                        int ReplacingAGroup = neighbors[rotate];
+                        switch (rotate)
+                        {
+                            case 0:
+                                field[X, Y] += 1;
+                                field[X, Y - 1] += 4;
+                                break;
+                            case 1:
+                                field[X, Y] += 2;
+                                field[X + 1, Y] += 8;
+                                break;
+                            case 2:
+                                field[X, Y] += 4;
+                                field[X, Y + 1] += 1;
+                                break;
+                            case 3:
+                                field[X, Y] += 8;
+                                field[X - 1, Y] += 2;
+                                break;
+                        }
+                        for (int i = 0; i < Width; i++)
+                            for (int j = 0; j < Height; j++)
+                                if (group[i, j] == ReplacingAGroup)
+                                    group[i, j] = group[X, Y];
+                        link = false;
+                    }
+                }
+                link = true;
+                for (int i = 0; i < Width; i++)
+                    for (int j = 0; j < Height; j++)
+                        if (group[i, j] != group[X, Y])
+                            link = false;
+                if (link)
+                    break;
+            }
+            Finish = new Point((Width - 1) / 2, 0);
+            Start = new Point((Width - 1) / 2, Height - 1);
+            Maze = field;
+        }
+    }
+}
